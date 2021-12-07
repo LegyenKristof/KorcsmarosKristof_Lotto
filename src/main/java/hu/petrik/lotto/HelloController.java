50763d01c97b80c11cb2e79e9e8abd66a65c4eaa
LegyenKristof <48032365+LegyenKristof@users.noreply.github.com>
2021-12-07 21:16:33 +0100

    Initial commit

---
+++ src/main/java/hu/petrik/lotto/HelloController.java
@@ -0,0 +1,17 @@
+package hu.petrik.lotto;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    public Button buttonSorsol;
+    @FXML
+    public Label labelSzam;
+    @FXML
+    public Label labelSzamok;
+
+
+}
